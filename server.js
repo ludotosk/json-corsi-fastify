d9423d392a8aae9bfc32008e7457028105d4ffe6
ludotosk <toscanoludovico@gmail.com>
2021-05-16 12:17:14 +0200

    creato server con fastify

---
+++ server.js
@@ -0,0 +1,104 @@
+const fs = require('fs')
+const fastify = require('fastify')()
+
+let rawdata = fs.readFileSync(require.resolve('./db.json'));
+let corsi = JSON.parse(rawdata);
+
+const port = process.env.PORT || 3000;
+
+fastify.get('/corsi', function (request, reply) {
+    query = request.query;
+
+    res = corsi.corsi;
+
+    if (query.t != undefined) {
+        res = res.filter(function (el) {
+            return el.t == query.t;
+        })
+    }
+
+    if (query.n != undefined) {
+        res = res.filter(function (el) {
+            return el.n == query.n;
+        })
+    }
+
+    if (query.u != undefined) {
+        res = res.filter(function (el) {
+            return el.u == query.u;
+        })
+    }
+
+    if (query.a != undefined) {
+        res = res.filter(function (el) {
+            return el.a == query.a;
+        })
+    }
+
+    if (query.c != undefined) {
+        res = res.filter(function (el) {
+            return el.c == query.c;
+        })
+    }
+
+    if (query.e != undefined) {
+        res = res.filter(function (el) {
+            return el.e == query.e;
+        })
+    }
+
+    if (query.s != undefined) {
+        res = res.filter(function (el) {
+            return el.s == query.s;
+        })
+    }
+
+    reply.send(JSON.stringify(res.sort((firstItem, secondItem) => firstItem.n - secondItem.n)));
+})
+
+fastify.get('/master', function (request, reply) {
+    query = request.query;
+
+    res = corsi.master;
+
+    if (query.uni != undefined) {
+        res = res.filter(function (el) {
+            return el.uni == query.uni;
+        })
+    }
+
+    if (query.corso != undefined) {
+        res = res.filter(function (el) {
+            return el.corso == query.corso;
+        })
+    }
+
+    if (query.citta != undefined) {
+        res = res.filter(function (el) {
+            return el.citta == query.citta;
+        })
+    }
+
+    if (query.lingua != undefined) {
+        res = res.filter(function (el) {
+            return el.lingua == query.lingua;
+        })
+    }
+
+    if (query.tipo != undefined) {
+        res = res.filter(function (el) {
+            return el.tipo == query.tipo;
+        })
+    }
+
+    reply.send(JSON.stringify(res.sort((firstItem, secondItem) => firstItem.n - secondItem.n)));
+})
+
+// Run the server!
+fastify.listen(port, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+})
